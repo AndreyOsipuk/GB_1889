0f9ea6eaeb915777688876837f21869fe94b79ff
Andrey Osipuk <andrey.o@list.ru>
2022-04-15 00:49:51 +0300

    change jest config

--- jest.config.js
+++ jest.config.js
@@ -82,7 +82,7 @@ module.exports = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '\\.(css)$': 'identity-obj-proxy',
+    '\\.(css|scss)$': 'identity-obj-proxy',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader

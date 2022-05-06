a4d56ca85af8d8a9c439b60eca9d726896b01e26
AndreyOsipuk <45951478+AndreyOsipuk@users.noreply.github.com>
2022-05-06 23:05:35 +0300

    Revert "Lesson 1"

--- src/components/FormClass/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export class Button extends Component {
-  render() {
-    return <button onClick={this.props.click}>{this.props.name}</button>
-  }
-}
--- src/components/FormClass/Input.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export class Input extends Component {
-  render() {
-    return <input type="text" value={this.props.value} onChange={this.props.change} />
-  }
-}
